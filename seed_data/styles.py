9965099b62f63480b8926705dbffc2817a5d4cd0
YoYoSuperStar <joanna.k27901@yahoo.co.uk>
2024-04-21 19:41:12 +0100

    basic set up

--- seed_data/styles.py
+++ seed_data/styles.py
@@ -1,22 +1,22 @@
 styles = {
     1: {
         "Name": "Action", 
-        "Description": "Action is an amazing thing and you are just a genius.",
+        "Description": "The action-oriented communicator is focused on results and performance. They want to see and hear about achievements and actions to overcome challenges. They tend to be direct, quick to move from one topic to another, pragmatic and impatient. They need to keep things moving.",
         "People": "Steve Jobs",
     },
     2: {
         "Name": "Process", 
-        "Description": "Process is an amazing thing and you are just a genius.",
+        "Description": "The process-oriented communicator is focused on facts, procedures, details and proof. They tend to be more systematic and logical, moving forward with caution. They want to see the plan in all its nitty gritty detail.",
         "People": "Bill Gates",
     },
     3: {
         "Name": "Idea", 
-        "Description": "An idea is an amazing thing and you are just a genius.",
+        "Description": "The idea-oriented communicator is focused on concepts and vision. They are creative and intrigued with new ways of doing things. They care about possibilities not details. They can be provocative and charismatic but also ego-centric. They prefer to focus on the big picture.",
         "People": "Elon Musk",
     },
     4: {
         "Name": "People", 
-        "Description": "People are cool and you are as well and you are just a genius.",
+        "Description": "The people-oriented communicator is focused on feelings, motivations, people and relationships in general. They tend to be perceptive and emotional, more sensitive and spontaneous. They need to know why and feel assured that people have what they need.",
         "People": "Jeff Bezos",
     }
 }
